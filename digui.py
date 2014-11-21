3440a6afea3c607e63065ac986253b13504ee15c
pan <543421341@qq.com>
2014-11-21 10:46:07 +0800

    递归函数

---
+++ digui.py
@@ -0,0 +1,13 @@
+# -*- coding: cp936 -*-
+#实用的递归函数
+def sumtree(L):
+	tot=0
+	for x in L:
+		if not isinstance(x,list):
+			tot+=x
+		else:
+			tot+=sumtree(x)
+	return tot
+			
+L=[1,[2,[3,4],5],6,[7,8]]
+print sumtree(L)	
