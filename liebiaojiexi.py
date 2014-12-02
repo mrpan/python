ab59d0b559a4b973c242ba09fcb6b130dddb7db7
pan <543421341@qq.com>
2014-12-02 09:42:02 +0800

    列表解析 嵌套测试

--- liebiaojiexi.py
+++ liebiaojiexi.py
@@ -1,3 +1,4 @@
+# -*- coding: cp936 -*-
 res=[ord(x) for x in "panxiaoming"]
 print res
 a=[x**2 for x in range(10)]
@@ -7,4 +8,5 @@ print b
 
 #列表解析有方括号封装起来，在方括号中编写一个表达式，其中的变量在 类似for循环中
 c=[x+y for x in 'pan' for y in 'xiaoming']
-print c
+d=c
+print d
